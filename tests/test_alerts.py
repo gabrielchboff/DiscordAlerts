f47e083ed3124987e1cb8e6492b45ccafa4482aa
gabrielchboff <gabrielchagasboff@gmail.com>
2023-08-01 15:17:30 -0300

    test: Building unit tests

---
+++ tests/test_alerts.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestAlerts:
+    def test_alert_erroring(self):
+        pass
+
+    def test_alert_warning(self):
+        pass
+
+    def test_alert_message(self):
+        pass
---
+++ tests/test_discord_alert.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+class TestDiscordAlert:
+    def test_instance(self):
+        pass
